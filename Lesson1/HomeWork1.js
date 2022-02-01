1c8ee7a541b9ea8bf432877238e86778d8035c0f
GrantThere <siris.gan@gmail.com>
2022-02-01 02:14:23 +0200

    Initial commit

---
+++ Lesson1/HomeWork1.js
@@ -0,0 +1,141 @@
+// - Створити змінні. Присвоїти кожному з них значення:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let hlo = 'hello';
+let ow = 'owu';
+let cm = 'com';
+let u = 'ua';
+let a = 1;
+let b = 10;
+let c = -999;
+let d = 123;
+let Pi = 3.14;
+let E = 2.7;
+let f = 16;
+let bool1 = true;
+let bool2 = false;
+// Вивести кожну змінну за допомогою:
+// console.log
+console.log(hlo);
+console.log(ow);
+console.log(cm);
+console.log(u);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(Pi);
+console.log(E);
+console.log(f);
+console.log(bool1);
+console.log(bool2);
+// alert
+// alert(hlo);
+// alert(ow);
+// alert(cm);
+// alert(u);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(Pi);
+// alert(E);
+// alert(f);
+// alert(bool1);
+// alert(bool2);
+
+// document.write
+document.write(`<div>${hlo}</div>`);
+document.write(`<div>${ow}</div>`);
+document.write(`<div>${cm}</div>`);
+document.write(`<div>${u}</div>`);
+document.write(`<div>${a}</div>`);
+document.write(`<div>${b}</div>`);
+document.write(`<div>${c}</div>`);
+document.write(`<div>${d}</div>`);
+document.write(`<div>${Pi}</div>`);
+document.write(`<div>${E}</div>`);
+document.write(`<div>${f}</div>`);
+document.write(`<div>${bool1}</div>`);
+document.write(`<div>${bool2}</div>`);
+//  Переприсвоїти кожній змінній з завдання значення на довільне.
+hlo = 666;
+ow = true;
+cm = 'commend';
+u = 12345654321;
+a = false;
+b = false;
+c = 5*8;
+d = '123';
+Pi = 'Pipipipip';
+E = 5+8;
+f = 16;
+bool1 = 'true';
+bool2 = 'false';
+// Вивести кожну змінну за допомогою:
+// console.log
+console.log(hlo);
+console.log(ow);
+console.log(cm);
+console.log(u);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(Pi);
+console.log(E);
+console.log(f);
+console.log(bool1);
+console.log(bool2);
+// alert
+// alert(hlo);
+// // alert(ow);
+// // alert(cm);
+// // alert(u);
+// // alert(a);
+// // alert(b);
+// // alert(c);
+// // alert(d);
+// // alert(Pi);
+// // alert(E);
+// // alert(f);
+// // alert(bool1);
+// // alert(bool2);
+// document.write
+document.write(`<div></div>`);
+document.write(`<div></div>`);
+document.write(`<div></div>`);
+document.write(`<div>${hlo}</div>`);
+document.write(`<div>${ow}</div>`);
+document.write(`<div>${cm}</div>`);
+document.write(`<div>${u}</div>`);
+document.write(`<div>${a}</div>`);
+document.write(`<div>${b}</div>`);
+document.write(`<div>${c}</div>`);
+document.write(`<div>${d}</div>`);
+document.write(`<div>${Pi}</div>`);
+document.write(`<div>${E}</div>`);
+document.write(`<div>${f}</div>`);
+document.write(`<div>${bool1}</div>`);
+document.write(`<div>${bool2}</div>`);
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person. (Не об'єкт)
+let person = {
+    firstName: 'Іван',
+    middleName: 'Ростиславович',
+    lastName: 'Галан'
+}
+//За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// prompt(person.firstName);
+// prompt(person.middleName);
+// prompt(25);
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+prompt('Вітаю' + person.firstName + person.middleName + '.' + ' Тобі 25 років.' );
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+
